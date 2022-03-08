5739de0e379b1fa0171583ef90eb41ac6d27a143
Tristan Slater <hello@trslater.ca>
2022-03-08 13:07:53 -0800

    Add camera

---
+++ raytracer/camera.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from functools import cached_property
+from math import tan
+
+from .geometry import Point3D
+
+
+@dataclass(frozen=True)
+class Camera:
+    position: Point3D
+    aspect: float
+    fov: float
+    """Vertical fov"""
+    near_clip: float
+    far_clip: float
+
+    @cached_property
+    def image_height(self) -> float:
+        return 2*self.near_clip*tan(self.fov/2)
+
+    @cached_property
+    def image_width(self) -> float:
+        return self.image_height*self.aspect
+
+    @cached_property
+    def image_origin(self):
+        # For now, to simplify, camera always looks in positive z direction
+        return Point3D(self.position.x - self.image_width/2,
+                       self.position.y + self.image_height/2,
+                       self.position.z + self.far_clip)
