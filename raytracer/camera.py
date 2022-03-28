d0b306ff328f9b5675a544a241e31b474dd68726
Tristan Slater <hello@trslater.ca>
2022-03-28 12:44:31 -0700

    Clean up

--- raytracer/camera.py
+++ raytracer/camera.py
@@ -16,26 +16,9 @@ class Camera:
     far_clip: float
 
     @cached_property
-    def image_height(self) -> float:
+    def near_clip_height(self) -> float:
         return 2*self.near_clip*tan(self.vfov/2)
 
     @cached_property
-    def image_width(self) -> float:
-        return self.image_height*self.aspect
-
-    @cached_property
-    def image_origin(self):
-        # For now, to simplify, camera always looks in negative z direction
-        # Origin in center of image
-        return Point3D(self.position.x, self.position.y,
-                       self.position.z - self.near_clip)
-
-    @cached_property
-    def near_plane(self) -> Plane:
-        return Plane(self.image_origin, Point3D(0, 0, -1))
-
-    @cached_property
-    def far_plane(self) -> Plane:
-        return Plane(Point3D(self.position.x, self.position.y,
-                             self.position.z - self.far_clip),
-                     Point3D(0, 0, -1))
+    def near_clip_width(self) -> float:
+        return self.near_clip_height*self.aspect
