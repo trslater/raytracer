7483737d10a69004edc47b491688d0fdd11427c6
Tristan Slater <hello@trslater.ca>
2022-03-30 11:44:26 -0700

    Simplify camera to always be on z-axis

--- raytracer/camera.py
+++ raytracer/camera.py
@@ -2,18 +2,22 @@ from dataclasses import dataclass
 from functools import cached_property
 from math import tan
 
-from .geometry import Plane, Point3D
+from .geometry import Plane, Point2D, Point3D
 
 
 @dataclass(frozen=True)
 class Camera:
     """Near/far clip should be magnitudes. They will be """
     
-    position: Point3D
+    distance: float
     aspect: float
     vfov: float
     near_clip: float
     far_clip: float
+
+    @cached_property
+    def position(self) -> Point3D:
+        return Point3D(0, 0, -self.distance)
 
     @cached_property
     def near_clip_height(self) -> float:
