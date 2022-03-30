fc547e1ae3b0f66c49e32751160d54e43b2d7804
Tristan Slater <hello@trslater.ca>
2022-03-30 11:10:51 -0700

    Fix: FOV using degrees instead of radians

--- examples/sphere.py
+++ examples/sphere.py
@@ -1,3 +1,4 @@
+import math
 from raytracer.camera import Camera
 from raytracer.geometry import Point3D, Sphere
 from raytracer.rendering import Image
@@ -6,7 +7,7 @@ from raytracer.scene import Scene
 
 def main():
     s = Scene((Sphere(Point3D(0, 0, 0), 3),))
-    c = Camera(Point3D(0, 0, 5), 1, 45, 1, 100)
+    c = Camera(Point3D(0, 0, 5), 1, math.pi/2, 1, 100)
     i = Image(s, c, width=100)
     
     i.render()
