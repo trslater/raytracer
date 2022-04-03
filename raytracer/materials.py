df6f2acfb626c6c943047b0fda789ea54d5ac87c
Tristan Slater <hello@trslater.ca>
2022-04-03 09:25:46 -0700

    Add a very basic material

---
+++ raytracer/materials.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+from .color import Color
+
+
+@dataclass(frozen=True)
+class Material:
+    color: Color
+    diffusion: float
+    specularity: float
+    shininess: float
